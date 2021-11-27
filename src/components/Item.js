94497142b244b6a81bd50a178de7a55047565e6a
Jake <jakerfilut@gmail.com>
2021-11-27 15:28:12 -0700

    commit tree db1ad94d6278d3abfa57221dbae4702381f0c3ff

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+const [currentAddCart, setAddCart] = useState(false)
+
+function addToCartButton(){
+  setAddCart((currentAddCart) => !currentAddCart)
+
+}
+
   return (
-    <li className="">
+    <li className={currentAddCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addToCartButton}className="add">
+        {currentAddCart ? "Remove from Cart": "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// function Item({ name, category }) {
+//   return (
+//     <li className="">
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add">Add to Cart</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
